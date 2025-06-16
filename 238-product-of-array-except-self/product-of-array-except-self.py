6168d8862775c2b75576ffb961bdcae2d25fbbb1
Danzel Ong <danzelongwj01@gmail.com>
2025-06-16 16:09:51 +0800

    Time: 33 ms (29.39%) | Memory: 23.2 MB (79.71%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.py
@@ -0,0 +1,20 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # initialize res array,
+        # where each index stores product of all nums except nums[i]
+        res = [1] * len(nums)
+
+        # intialize prefix and suffix running products
+        prefixProduct = suffixProduct = 1
+
+        for i in range(len(nums)):
+            # multiple current index with product of all nums to its left,
+            # and accumulate prefix running product
+            res[i] *= prefixProduct
+            
+            # mirror the same for the suffix
+            prefixProduct *= nums[i]
+            res[-i - 1] *= suffixProduct
+            suffixProduct *= nums[-i - 1]
+        
+        return res
