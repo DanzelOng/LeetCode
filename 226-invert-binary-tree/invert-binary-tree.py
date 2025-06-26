e2971074bdecf97980bb41f6e7a40a32e50745c7
Danzel Ong <danzelongwj01@gmail.com>
2025-06-26 15:46:43 +0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (31.49%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.py
+++ 226-invert-binary-tree/invert-binary-tree.py
@@ -15,10 +15,12 @@ class Solution:
             node = stack.pop()
             node.left, node.right = node.right, node.left
 
-            if node.right:
-                stack.append(node.right)
             
             if node.left:
                 stack.append(node.left)
+
+                
+            if node.right:
+                stack.append(node.right)
             
         return root
