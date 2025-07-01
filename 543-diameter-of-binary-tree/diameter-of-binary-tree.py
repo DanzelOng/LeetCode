0d638f3e2c4be14a969200061c3fca7df3b8aeaf
Danzel Ong <danzelongwj01@gmail.com>
2025-07-01 18:14:02 +0800

    Time: 1 ms (98.39%) | Memory: 20.8 MB (49.32%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -1,27 +1,27 @@
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        # keeps track of the maximum diameter
         maxDiameter = 0
-        
-        # preorder
-        def preorderDfs(node):
+
+        # helper function used to post-orderly traverse the tree
+        def postorderDfs(node):
             nonlocal maxDiameter
 
+            # an empty tree (NULL) has no edge
             if node is None:
                 return 0
             
-            leftDepth = preorderDfs(node.left)
-            rightDepth = preorderDfs(node.right)
-            maxDiameter = max(maxDiameter, leftDepth + rightDepth)
-            return max(leftDepth, rightDepth) + 1   
-        
-        preorderDfs(root)
-        return maxDiameter
+            # recursively compute no. of edges for left and right subtrees of current node
+            leftEdges = postorderDfs(node.left)
+            rightEdges = postorderDfs(node.right)
+            
+            # compute current diameter and update maximum diameter if needed
+            # diameter = left subtree depth + right subtree depth (in edges)
+            maxDiameter = max(maxDiameter, leftEdges + rightEdges)
 
+            # return the greater depth (in edges) of the 2 subtrees of the current node
+            # +1 accounts for the additional edge from the current node to its parent
+            return max(leftEdges, rightEdges) + 1   
         
+        postorderDfs(root)
+        return maxDiameter       
