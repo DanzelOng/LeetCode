baa6da0078bb37d17aef83ec7aea780ccb75818b
Danzel Ong <danzelongwj01@gmail.com>
2025-07-08 12:17:46 +0800

    Time: 48 ms (63.54%) | Memory: 19.2 MB (79.05%) - LeetSync

--- 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.py
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.py
@@ -1,22 +1,34 @@
 class Solution:
     def postorder(self, root: 'Node') -> List[int]:
-        def dfs(node):
+        def traverse(node):
             if not node:
                 return []
 
+            # initialize the stack with the tuple (root, visited_flag)
             stack = [( node, False )]
+
+            # array to store postorder traversal of values
             res = []
             
             while stack:
                 nde, visited = stack.pop()
 
+                # check if the node has children and if it is not yet visited
                 if nde.children and not visited:
+
+                    # mark the node as visited
                     stack.append(( nde, True ))
+
+                    # add all children nodes in reversed order to the stack
+                    # this allows the first child of the node to be processed
                     for i in range(len(nde.children) - 1, -1, -1):
                         stack.append(( nde.children[i], False ))
+
+                # add the node value if it has no children or if it is already visited
                 else:
                     res.append(nde.val)
             
+            # return the postorder traversal array output
             return res
 
-        return dfs(root)
+        return traverse(root)
